df04448d9d93428e32c4c1fbe65637e08d5c81e3
priyanka mohanty <117801319+SDEPriyanka@users.noreply.github.com>
2023-08-26 23:16:18 +0530

    updated assignment-6

--- assignment-6.js
+++ assignment-6.js
@@ -1,4 +1,20 @@
-// print sqrt of number
-
-arr = [4,15,7,3,9,5]
-
+// show all employee details
+// Show employee sal > 23000
+ 
+const employee = [
+    {
+        name:"abc",
+        sal:23000,
+        age:34
+    },
+    {
+        name:"pqr",
+        sal:25000,
+        age:30
+    },
+    {
+        name:"mnc",
+        sal:20000,
+        age:30
+    }
+]
