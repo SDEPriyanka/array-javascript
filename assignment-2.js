831c47831be965dee8877dafc65e8a5ca5069ba9
priyanka mohanty <117801319+SDEPriyanka@users.noreply.github.com>
2023-08-26 23:09:46 +0530

    assignment-2 completed
    assignment-2 completed .....

--- assignment-2.js
+++ assignment-2.js
@@ -2,3 +2,8 @@
 // Remove first element of array 
 
 arr = [5, 12, 61, 19, 12, 72, 20, 55];
+arr.push(100);
+console.log("add 100 at last element",arr);
+
+arr.shift();
+console.log("remove 1st element",arr);
--- assignment-3.js
+++ assignment-3.js
@@ -1,3 +1,16 @@
 // What is diffecrence between PUSH and POP
+// PUSH = ADDs new element at the end of an array. 
+let arry = ['ay','gu','tp','yw'];
+arry.push('bb');
+console.log(arry);
+// POP = Remove last element  of an array.
+let array = ['ay','gu','tp','yw'];
+array.pop()
+console.log(array);
 // What is array  splice vs slice
+
+
 // What is array indexOf
+// The Indexof() method serches the spesified array element position.
+let arr = ['a','g','t','y'];
+console.log(arr.indexOf('t'));
